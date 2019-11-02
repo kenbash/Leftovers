e068de1d54ba90b0f673f49af98038e3ae233de0
Ken Ashley <ken.ashley97@yahoo.com>
2019-11-02 12:14:24 -0700

    Setup basic client layout/face switching

--- src/client/App.js
+++ src/client/App.js
@@ -2,16 +2,85 @@ import React, { Component } from 'react';
 import './App.scss';
 
 export default class App extends Component {
-  state = { data: null };
+  state = { faceClass: 'home-face' };
 
   componentDidMount() {
-    fetch('/api/meal')
-      .then(res => res.json())
-      .then(res => this.setState({ data: res.meal }));
+    // fetch('/api/meal?name=SteakDinner')
+    //   .then(res => res.json())
+    //   .then((res) => {
+    //     console.log(res);
+    //   });
+    // const test = JSON.stringify({
+    //   name: 'testFood1234',
+    //   servings: 3
+    // });
+    // fetch('/api/meal/create', {
+    //   method: 'POST',
+    //   headers: { 'Content-Type': 'application/json' },
+    //   body: test
+    // }).then(res => console.log(res));
+  }
+
+  changeFace(dir) {
+    const { faceClass } = this.state;
+    let newFace;
+
+    // export faceClass to enum later?
+    switch (faceClass) {
+      case 'home-face':
+        newFace = dir ? 'list-face' : 'detail-face';
+        break;
+      case 'list-face':
+        newFace = dir ? 'detail-face' : 'home-face';
+        break;
+      case 'detail-face':
+        newFace = dir ? 'home-face' : 'list-face';
+        break;
+      default:
+        newFace = faceClass;
+        break;
+    }
+
+    this.setState({
+      faceClass: newFace
+    });
   }
 
   render() {
-    const { data } = this.state;
-    return <div>{data ? <p>{`${data}`}</p> : <p>Loading</p>}</div>;
+    const { faceClass } = this.state;
+    return (
+      <React.Fragment>
+        <Header />
+        <main>
+          <section className="temp">
+            <button type="button" onClick={() => this.changeFace(false)}>
+              Left
+            </button>
+            <button type="button" onClick={() => this.changeFace(true)}>
+              Right
+            </button>
+          </section>
+          <section className={faceClass}>
+            <div className="meal-grid">
+              <h1>Grid</h1>
+            </div>
+            <div className="meal-list">
+              <h1>List</h1>
+            </div>
+            <div className="meal-detail">
+              <h1>Detail</h1>
+            </div>
+          </section>
+        </main>
+      </React.Fragment>
+    );
   }
+}
+
+function Header() {
+  return (
+    <header>
+      <h1>Leftovers</h1>
+    </header>
+  );
 }
