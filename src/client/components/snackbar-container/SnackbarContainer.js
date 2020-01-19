68cb7bd35b1f712cbca164075396685ebe89600d
Ken Ashley <ken.ashley97@yahoo.com>
2020-01-18 18:30:30 -0800

    Implemented snackbar queueing for consecutive messages

--- src/client/components/snackbar-container/SnackbarContainer.js
+++ src/client/components/snackbar-container/SnackbarContainer.js
@@ -3,29 +3,33 @@ import { Snackbar } from '@material-ui/core';
 import { Alert, AlertTitle } from '@material-ui/lab';
 import { onSnackbarSent } from '../../services/SnackbarService';
 
-const DURATION = 6000; // amount of time before snackbar closes
-
 class SnackbarContainer extends Component {
+  messagQueue = [];
+
   constructor(props) {
     super(props);
 
     this.state = {
       open: false,
-      type: 'info', // 'success' | 'info' | 'warning' | 'error'
-      title: null,
-      text: null
+      message: {
+        key: undefined,
+        type: 'info', // 'success' | 'info' | 'warning' | 'error'
+        title: undefined,
+        text: undefined
+      }
     };
   }
 
   componentDidMount() {
     onSnackbarSent((props) => {
-      const { type, title, text } = props;
-      this.setState({
-        open: true,
-        type: type || 'info',
-        title,
-        text
-      });
+      this.messagQueue.push({ ...props, key: new Date().getTime() });
+
+      const { open } = this.state;
+      if (open) {
+        this.setState({ open: false });
+      } else {
+        this.processQueue();
+      }
     });
   }
 
@@ -34,14 +38,31 @@ class SnackbarContainer extends Component {
     this.setState({ open: false });
   }
 
+  handleExited = () => {
+    this.processQueue();
+  }
+
+  processQueue() {
+    if (this.messagQueue.length > 0) {
+      this.setState({ open: true, message: this.messagQueue.shift() });
+    }
+  }
+
   render() {
+    const { open, message } = this.state;
     const {
-      open, type, title, text
-    } = this.state;
+      key, type, title, text
+    } = message;
 
     return (
       <div className="snackbar-wrapper">
-        <Snackbar open={open} onClose={this.handleClose} autoHideDuration={DURATION}>
+        <Snackbar
+          key={key}
+          open={open}
+          onClose={this.handleClose}
+          onExited={this.handleExited}
+          autoHideDuration={6000}
+        >
           <Alert elevation={6} variant="filled" severity={type} onClose={this.handleClose}>
             {title ? <AlertTitle>{title}</AlertTitle> : null}
             {text}
